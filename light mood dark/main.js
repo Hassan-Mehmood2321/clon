f1ffaf6f56cfcc89975fc6a002447ad00f07cc9b
Hassan Mehmood <khanhameez838@gmail.com>
2025-06-28 13:40:40 -0700

    new

---
+++ light mood dark/main.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const body = document.body;
+
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+    }
+}
